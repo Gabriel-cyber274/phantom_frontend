9e52bb49b2fa813de9051538400ab5f1feaeb440
imoh gabriel <gabrielimoh30@gmail.com>
2023-07-03 13:32:50 +0100

    leaderboard update1

--- app/leaderboard/Content.jsx
+++ app/leaderboard/Content.jsx
@@ -90,7 +90,7 @@ function Content() {
                                     <div className='frame position-relative'>
                                         <div className='poss d-flex justify-content-center align-items-center position-absolute'>3</div>
                                     </div>
-                                    <h2>{displayData[2].name}</h2>
+                                    <h2>{displayData[2].name.includes(' ')? displayData[2].name.slice(0, displayData[2].name.indexOf(' ')) : displayData[2].name}</h2>
                                     <span className='d-flex align-items-center'><svg className='me-1' width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5 10C7.76142 10 10 7.76142 10 5C10 2.23858 7.76142 0 5 0C2.23858 0 0 2.23858 0 5C0 7.76142 2.23858 10 5 10Z" fill="#FFDE07"/>
                                     <path d="M5 8.75C7.07107 8.75 8.75 7.07107 8.75 5C8.75 2.92893 7.07107 1.25 5 1.25C2.92893 1.25 1.25 2.92893 1.25 5C1.25 7.07107 2.92893 8.75 5 8.75Z" stroke="#505050" stroke-width="0.5"/>
@@ -103,7 +103,7 @@ function Content() {
                                     <div className='frame position-relative'>
                                         <div className='poss d-flex justify-content-center align-items-center position-absolute'>1</div>
                                     </div>
-                                    <h2>{displayData[0].name}</h2>
+                                    <h2>{displayData[0].name.includes(' ')? displayData[0].name.slice(0, displayData[0].name.indexOf(' ')) : displayData[0].name}</h2>
                                     <span className='d-flex align-items-center'>
                                     <svg className='me-1' width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M6.5 13C10.0899 13 13 10.0899 13 6.5C13 2.91015 10.0899 0 6.5 0C2.91015 0 0 2.91015 0 6.5C0 10.0899 2.91015 13 6.5 13Z" fill="#FFDE07"/>
@@ -118,7 +118,7 @@ function Content() {
                                     <div className='frame position-relative'>
                                         <div className='poss d-flex justify-content-center align-items-center position-absolute'>2</div>
                                     </div>
-                                    <h2>{displayData[1].name}</h2>
+                                    <h2>{displayData[1].name.includes(' ')? displayData[1].name.slice(0, displayData[1].name.indexOf(' ')) : displayData[1].name}</h2>
                                     <span className='d-flex align-items-center'>
                                     <svg className='me-1' width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M5 10C7.76142 10 10 7.76142 10 5C10 2.23858 7.76142 0 5 0C2.23858 0 0 2.23858 0 5C0 7.76142 2.23858 10 5 10Z" fill="#FFDE07"/>
@@ -151,7 +151,7 @@ function Content() {
                                     <h2>{idx + 4}</h2>
                                     <div className='d-flex back justify-content-between align-items-center'>
                                         <span></span>
-                                        <h3>{user.name}</h3>
+                                        <h3>{user.name.includes(' ')? user.name.slice(0, user.name.indexOf(' ')) : user.name}</h3>
                                         <h4 className='d-flex align-items-center'><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M9.5 18C14.7467 18 19 13.9706 19 9C19 4.02944 14.7467 0 9.5 0C4.25329 0 0 4.02944 0 9C0 13.9706 4.25329 18 9.5 18Z" fill="#FFDE07"/>
                                         <path d="M9.5 15.75C13.5041 15.75 16.75 12.7279 16.75 9C16.75 5.27208 13.5041 2.25 9.5 2.25C5.49594 2.25 2.25 5.27208 2.25 9C2.25 12.7279 5.49594 15.75 9.5 15.75Z" stroke="#505050" stroke-width="0.5"/>
@@ -168,7 +168,7 @@ function Content() {
                                     <h2>{idx + 4}</h2>
                                     <div className='d-flex back justify-content-between align-items-center'>
                                         <span></span>
-                                        <h3>{user.name}</h3>
+                                        <h3>{user.name.includes(' ')? user.name.slice(0, user.name.indexOf(' ')) : user.name}</h3>
                                         <h4 className='d-flex align-items-center'><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M9.5 18C14.7467 18 19 13.9706 19 9C19 4.02944 14.7467 0 9.5 0C4.25329 0 0 4.02944 0 9C0 13.9706 4.25329 18 9.5 18Z" fill="#FFDE07"/>
                                         <path d="M9.5 15.75C13.5041 15.75 16.75 12.7279 16.75 9C16.75 5.27208 13.5041 2.25 9.5 2.25C5.49594 2.25 2.25 5.27208 2.25 9C2.25 12.7279 5.49594 15.75 9.5 15.75Z" stroke="#505050" stroke-width="0.5"/>
@@ -185,7 +185,7 @@ function Content() {
                                     <h2>{idx + 4}</h2>
                                     <div className='d-flex back justify-content-between align-items-center'>
                                         <span></span>
-                                        <h3>{user.name}</h3>
+                                        <h3>{user.name.includes(' ')? user.name.slice(0, user.name.indexOf(' ')) : user.name}</h3>
                                         <h4 className='d-flex align-items-center'><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M9.5 18C14.7467 18 19 13.9706 19 9C19 4.02944 14.7467 0 9.5 0C4.25329 0 0 4.02944 0 9C0 13.9706 4.25329 18 9.5 18Z" fill="#FFDE07"/>
                                         <path d="M9.5 15.75C13.5041 15.75 16.75 12.7279 16.75 9C16.75 5.27208 13.5041 2.25 9.5 2.25C5.49594 2.25 2.25 5.27208 2.25 9C2.25 12.7279 5.49594 15.75 9.5 15.75Z" stroke="#505050" stroke-width="0.5"/>
@@ -202,7 +202,7 @@ function Content() {
                                 <h2>{displayData.findIndex(user=> user.id == JSON.parse(localStorage.currentUser).user.id)+1}</h2>
                                 <div className='d-flex back justify-content-between align-items-center'>
                                     <span></span>
-                                    <h3>{displayData.filter(user=> user.id == JSON.parse(localStorage.currentUser).user.id)[0].name}</h3>
+                                    <h3>{displayData.filter(user=> user.id == JSON.parse(localStorage.currentUser).user.id)[0].name.includes(' ')? displayData.filter(user=> user.id == JSON.parse(localStorage.currentUser).user.id)[0].name.slice(0, displayData.filter(user=> user.id == JSON.parse(localStorage.currentUser).user.id)[0].name.indexOf(' ')) : displayData.filter(user=> user.id == JSON.parse(localStorage.currentUser).user.id)[0].name}</h3>
                                     <h4 className='d-flex align-items-center'><svg width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M9.5 18C14.7467 18 19 13.9706 19 9C19 4.02944 14.7467 0 9.5 0C4.25329 0 0 4.02944 0 9C0 13.9706 4.25329 18 9.5 18Z" fill="#FFDE07"/>
                                     <path d="M9.5 15.75C13.5041 15.75 16.75 12.7279 16.75 9C16.75 5.27208 13.5041 2.25 9.5 2.25C5.49594 2.25 2.25 5.27208 2.25 9C2.25 12.7279 5.49594 15.75 9.5 15.75Z" stroke="#505050" stroke-width="0.5"/>
